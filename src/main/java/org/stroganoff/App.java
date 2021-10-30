064408731f5426f5e8901b7eb90400a61e9eff67
Sergey Stroganov <111sergey90@gmail.com>
2021-10-30 23:31:03 +0300

    Added XML Verification.

--- src/main/java/org/stroganoff/App.java
+++ src/main/java/org/stroganoff/App.java
@@ -2,11 +2,10 @@ package org.stroganoff;
 
 import org.apache.log4j.Logger;
 import org.stroganoff.exceptions.XmlFileException;
-import org.stroganoff.utils.JSONParser;
-import org.stroganoff.utils.JSONUtil;
-import org.stroganoff.utils.XMLConverter;
+import org.stroganoff.utils.*;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  * JSON Parser.
@@ -16,18 +15,31 @@ import java.io.IOException;
 public class App {
 
     public static final String FILE_PATH = "src/main/resources/stones.json";
-    public static final String XML_FILE = "/stones.xml";
+    public static final String XML_FILE = "src/main/resources/stones.xml";
+    public static final String XML_SCHEMA_FILE = "src/main/resources/stones_schema.xsd";
+    public static final String VERIFIED_SUCCESSFUL = "XML file verified successful";
+    public static final String NOT_VERIFIED = "XML file not verified, exit";
     public static final Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
+
+        XMLVerification xmlVerification = new XMLVerifier("xmlVerification.log");
+        if (xmlVerification.verificationToXSD(XML_FILE, XML_SCHEMA_FILE)) {
+            logger.info(XML_FILE + " - " + VERIFIED_SUCCESSFUL);
+        } else {
+            logger.error(NOT_VERIFIED);
+            System.exit(1);
+        }
+
         XMLConverter xmlConverter = new XMLConverter();
         String xmlDataString = null;
         try {
-            xmlDataString = xmlConverter.xmlFileResourceToString(XML_FILE);
+            xmlDataString = xmlConverter.xmlFileResourceToString("/stones.xml");
         } catch (XmlFileException e) {
             logger.error(e.getMessage());
             System.exit(1);
         }
+
         String jsonString = xmlConverter.convertXMLToJSON(xmlDataString);
         JSONUtil jsonUtil = new JSONUtil();
         jsonUtil.jsonSaveToFile(FILE_PATH, jsonString);
