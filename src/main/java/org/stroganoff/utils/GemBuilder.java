c2b8e7136aff98b69d04bb94689abe0e74d4ec2f
Sergey Stroganov <111sergey90@gmail.com>
2021-10-31 12:19:49 +0300

    NullPointerException fixed.

--- src/main/java/org/stroganoff/utils/GemBuilder.java
+++ src/main/java/org/stroganoff/utils/GemBuilder.java
@@ -2,11 +2,15 @@ package org.stroganoff.utils;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import org.apache.log4j.Logger;
 import org.stroganoff.entities.*;
+
+import java.util.Arrays;
 
 public class GemBuilder {
 
     private JsonObject currentJsonObject;
+    private final Logger LOGGER = Logger.getLogger(GemBuilder.class);
 
     public GemBuilder(JsonObject currentJsonObject) {
         this.currentJsonObject = currentJsonObject;
@@ -17,27 +21,39 @@ public class GemBuilder {
     }
 
     public Gem buildGem() {
-        Gem gemInstance;
-        String name = currentJsonObject.getAsJsonPrimitive("name").getAsString();
-        JsonPrimitive preciousnessPrimitive = currentJsonObject.getAsJsonPrimitive("preciousness");
-        Preciousness preciousness = Preciousness.valueOf(preciousnessPrimitive.getAsString());
-        JsonPrimitive miningLocationPrimitive = currentJsonObject.getAsJsonPrimitive("miningLocation");
-        String miningLocation = miningLocationPrimitive.getAsString();
-        JsonObject visualParametersJSONObject = currentJsonObject.getAsJsonObject("visualParameters");
-        VisualParameters visualParameters = buildVisualParametersInstance(visualParametersJSONObject);
-        JsonPrimitive stoneWeightPrimitive = currentJsonObject.getAsJsonPrimitive("stoneWeight");
-        double stoneWeight = stoneWeightPrimitive.getAsDouble();
-        gemInstance = new Gem(name, preciousness, miningLocation, visualParameters, stoneWeight);
+        Gem gemInstance = null;
+        try {
+            String name = currentJsonObject.getAsJsonPrimitive("name").getAsString();
+            JsonPrimitive preciousnessPrimitive = currentJsonObject.getAsJsonPrimitive("preciousness");
+            Preciousness preciousness = Preciousness.valueOf(preciousnessPrimitive.getAsString());
+            JsonPrimitive miningLocationPrimitive = currentJsonObject.getAsJsonPrimitive("miningLocation");
+            String miningLocation = miningLocationPrimitive.getAsString();
+            JsonObject visualParametersJSONObject = currentJsonObject.getAsJsonObject("visualParameters");
+            VisualParameters visualParameters = buildVisualParametersInstance(visualParametersJSONObject);
+            JsonPrimitive stoneWeightPrimitive = currentJsonObject.getAsJsonPrimitive("stoneWeight");
+            double stoneWeight = stoneWeightPrimitive.getAsDouble();
+            gemInstance = new Gem(name, preciousness, miningLocation, visualParameters, stoneWeight);
+        } catch (NullPointerException e) {
+            LOGGER.error(e.toString());
+            Arrays.stream(e.getStackTrace()).forEach(LOGGER::error);
+        }
+
         return gemInstance;
     }
 
     public VisualParameters buildVisualParametersInstance(JsonObject visualParameters) {
-        JsonPrimitive transparencyPrimitive = visualParameters.getAsJsonPrimitive("transparency");
-        Transparency transparency = Transparency.valueOf(transparencyPrimitive.getAsString());
-        JsonPrimitive colorPrimitive = visualParameters.getAsJsonPrimitive("color");
-        Color color = Color.valueOf(colorPrimitive.getAsString());
-        JsonPrimitive facesNumberPrimitive = visualParameters.getAsJsonPrimitive("facesNumber");
-        int facesNumber = facesNumberPrimitive.getAsInt();
-        return new VisualParameters(color, transparency, facesNumber);
+        try {
+            JsonPrimitive transparencyPrimitive = visualParameters.getAsJsonPrimitive("transparency");
+            Transparency transparency = Transparency.valueOf(transparencyPrimitive.getAsString());
+            JsonPrimitive colorPrimitive = visualParameters.getAsJsonPrimitive("color");
+            Color color = Color.valueOf(colorPrimitive.getAsString());
+            JsonPrimitive facesNumberPrimitive = visualParameters.getAsJsonPrimitive("facesNumber");
+            int facesNumber = facesNumberPrimitive.getAsInt();
+            return new VisualParameters(color, transparency, facesNumber);
+        } catch (NullPointerException e) {
+            LOGGER.error(e.getMessage());
+            Arrays.stream(e.getStackTrace()).forEach(LOGGER::error);
+            return null;
+        }
     }
 }
